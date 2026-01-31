ef3d18cbe40281808df29ae0522131601e1fb528
LeonardoGalvaoToffoli <leonardogalvao_@hotmail.com>
2026-01-31 00:50:31 -0300

    fix : main

--- src/main/java/br/com/hello/HelloApplication.java
+++ src/main/java/br/com/hello/HelloApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloApplication {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
 
